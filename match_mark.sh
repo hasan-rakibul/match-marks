11ef8c6dcc48b49b6a86d6988f5c93ac13d89c9b
Md Rakibul Hasan <rakibul.eeekuet@gmail.com>
2023-09-18 15:54:25 +0800

    sh added

---
+++ match_mark.sh
@@ -0,0 +1,5 @@
+python match.py \
+--lms gc_2023_2_COMP1002_V1_L1_A1_INT_772330_fullgc_2023-09-18-15-27-36.csv \
+--lms_col 12 \
+--local DSAmarking_Semester_2_2023.xlsx \
+--local_col 19                                                                                                                        

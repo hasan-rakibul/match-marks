de64ec4baac2f59cb65b4902eaebab771878193d
Md Rakibul Hasan <rakibul.eeekuet@gmail.com>
2023-10-10 15:14:01 +0800

    bash input

--- match_mark.sh
+++ match_mark.sh
@@ -1,5 +1,8 @@
+read -p "lms_col: " lms_col
+read -p "local_col: " local_col
+
 python match.py \
---lms gc_2023_2_COMP1002_V1_L1_A1_INT_772330_fullgc_2023-09-18-15-27-36.csv \
---lms_col 12 \
+--lms gc_2023_2_COMP1002_V1_L1_A1_INT_772330_fullgc_2023-10-10-14-18-48.csv \
+--lms_col $lms_col \
 --local DSAmarking_Semester_2_2023.xlsx \
---local_col 19                                                                                                                        
+--local_col $local_col                                                                                                       

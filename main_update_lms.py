e2ab0516aa0db625e5e906f8a71c5d920e53b32f
Md Rakibul Hasan <rakibul.eeekuet@gmail.com>
2024-10-10 23:18:39 +0800

    not changing the lms index as well

--- main_update_lms.py
+++ main_update_lms.py
@@ -40,15 +40,15 @@ def main():
     local_file = input("Local XLSX file (default: local.xlsx): ").strip() or "local.xlsx"
 
     # Reading the files and making the usernames (col index 3) as the dataframe index. Some student ID are empty, so using username
-    lms=pd.read_csv(lms_file, index_col=3)
-    local=pd.read_excel(local_file, index_col=3)
+    # lms=pd.read_csv(lms_file, index_col=3)
+    lms = pd.read_csv(lms_file, index_col=None)
+    local = pd.read_excel(local_file, index_col=3)
     
     # convert the index dtype to str; there was a mismatch between lms and local
     local.index = local.index.map(str)
-    lms.index = lms.index.map(str)
     
-    # input: nth column whereas python is 0-based. Plus, I will make student_id as index (so, one column index is vanished there)
-    lms_col = lms.columns[lms_col-2]
+    lms_col = lms.columns[lms_col-1] # python is 0-based.
+    # for local, I will make student_id as index (so, another one column index is vanished there)
     local_col = local.columns[local_col-2]
 
     logging.info('CHECK CAREFULLY!! Comparing Between:')
@@ -67,27 +67,28 @@ def main():
         logging.info('Exiting...')
         return
 
-    for row in lms.itertuples():
-        if row.Index not in local.index:
-            logging.debug(f"Student ID {row.Index} is not in local file. Skipped.")
+    for index, row in lms.iterrows():
+        student_id = str(row["Student ID"]) # convert to string as local index is string
+        if student_id not in local.index:
+            logging.debug(f"Student ID {student_id} is not in local file. Skipped.")
             continue
         # convert marks to float
         try:
-            local_mark = float(local.loc[row.Index, local_col])
+            local_mark = float(local.loc[student_id, local_col])
         except:
-            logging.info(Back.RED + "Student ID " + row.Index + " has invalid mark formatting in local file: " + local.loc[row.Index, local_col] + ". Skipped." + Back.RESET)
+            logging.info(Back.RED + "Student ID " + student_id + " has invalid mark formatting in local file: " + local.loc[student_id, local_col] + ". Skipped." + Back.RESET)
             continue
         
         # empty marks are NaN
         # sometimes when some marks are only on LMS, we don't need to proceed if local mark is empty for a student
         if math.isnan(local_mark):
-            logging.debug(f"Student ID {row.Index} has probable empty mark in local file: {local_mark}. Skipped.")
+            logging.debug(f"Student ID {student_id} has probable empty mark in local file: {local_mark}. Skipped.")
             continue
         
-        lms_mark = lms.loc[row.Index, lms_col]
+        lms_mark = lms.loc[index, lms_col]
         if type(lms_mark) == str:
             if lms_mark == 'In Progress' or lms_mark == 'Needs Marking': # Those who submitted in LMS
-                logging.debug(f"Student ID {row.Index} has invalid marking in LMS file: {lms_mark}. Considered as 0.")
+                logging.debug(f"Student ID {student_id} has invalid marking in LMS file: {lms_mark}. Considered as 0.")
                 lms_mark = 0.0
             elif lms_mark[:13] == 'Needs Marking': # those who submitted after their marks are uploaded in LMS, marks would be 'Needs Marking(<marks>)'
                 lms_mark = lms_mark[14:-1] # extracting the marks. 13th is '(' and the end has ')'
@@ -95,18 +96,18 @@ def main():
         lms_mark = float(lms_mark)
         
         if math.isnan(lms_mark):
-            logging.debug(f"Student ID {row.Index} has empty mark in LMS file: {lms_mark}. Considered as 0.")
+            logging.debug(f"Student ID {student_id} has empty mark in LMS file: {lms_mark}. Considered as 0.")
             lms_mark = 0.0
 
         if lms_mark < local_mark:
-            lastname = lms.loc[row.Index, 'Last Name']
-            firstname = lms.loc[row.Index, 'First Name']
-            lms.loc[row.Index, lms_col] = local_mark
-            logging.info(f"{lastname}, {firstname} ({row.Index}): {lms_mark} --> {local_mark}. Updated.")
+            lastname = lms.loc[index, 'Last Name']
+            firstname = lms.loc[index, 'First Name']
+            lms.loc[index, lms_col] = local_mark
+            logging.info(f"{lastname}, {firstname} ({student_id}): {lms_mark} --> {local_mark}. Updated.")
         elif lms_mark > local_mark:
-            logging.info(f"Student ID {row.Index} has higher mark in LMS file: {lms_mark} > {local_mark}. Skipped.")
+            logging.info(f"Student ID {student_id} has higher mark in LMS file: {lms_mark} > {local_mark}. Skipped.")
 
-    save_as = "lms_updated.csv"    
+    save_as = "lms.csv"    
     lms.to_csv(save_as, index=False)
     logging.info(f"Updated LMS file saved as {save_as}")
 
